744b2c0a49469b51559826f9e95577a27fb242d5
sumeetsabata07 <202402028@student.xavier.ac.in>
2026-03-18 23:33:39 +0530

    Remove dotenv for Railway

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const https = require("https");
-require("dotenv").config();
+// dotenv not needed on Railway
 
 const app = express();
 
