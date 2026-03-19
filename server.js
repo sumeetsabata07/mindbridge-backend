6ec30b3f465c9ea2b7b3328d5326d8eed2864d0d
sumeetsabata07 <202402028@student.xavier.ac.in>
2026-03-19 12:56:34 +0530

    Fix port binding for Railway

--- server.js
+++ server.js
@@ -65,6 +65,6 @@ app.post("/chat", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("MindBridge backend running on port " + PORT);
 });
